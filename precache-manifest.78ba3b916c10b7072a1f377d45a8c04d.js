1d7cb38c725f3556c949bc37281167dfa66fd7eb
Arya Ramteke <aryaramteke@Aryas-MacBook-Air.local>
2023-09-01 21:13:44 +0530

    Updates

---
+++ precache-manifest.78ba3b916c10b7072a1f377d45a8c04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "076054e63543c56b16a7",
+    "url": "/static/css/main.b096cacf.chunk.css"
+  },
+  {
+    "revision": "076054e63543c56b16a7",
+    "url": "/static/js/main.465b12b2.chunk.js"
+  },
+  {
+    "revision": "dc9507c8f2fe9880d8a2",
+    "url": "/static/js/runtime~main.c165c877.js"
+  },
+  {
+    "revision": "9a411511c785bd401aca",
+    "url": "/static/js/2.16cbf300.chunk.js"
+  },
+  {
+    "revision": "ff7f2396e4d79702702a",
+    "url": "/static/js/3.a712f853.chunk.js"
+  },
+  {
+    "revision": "985c139806ea098622655a6840782f79",
+    "url": "/index.html"
+  }
+];
